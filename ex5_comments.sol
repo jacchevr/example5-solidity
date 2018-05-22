b12cbf21b8600e283d66d29f087ba54d32287945
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-22 10:37:03 -0600

    Update ex5_comments.sol

--- ex5_comments.sol
+++ ex5_comments.sol
@@ -1,1 +1,24 @@
-# example5-solidity
+pragma solidity 0.4.0;
+
+//single line comment
+
+/*
+write
+your
+comment
+over
+multiple
+lines
+*/
+
+/// natspec comment
+
+/**
+ * Title for a contract
+ * 
+ * Better description for a contract
+ * 
+ * @author Jacques Chevrier
+ * @section This is a section
+ * 
+ */
